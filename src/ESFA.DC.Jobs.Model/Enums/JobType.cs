1f302705b580a368ffb0a6cde64f5148cd4e18d9
John Youren <john.youren@gmail.com>
2018-11-15 10:45:34 +0000

    Change JobType enum to match database values

--- src/ESFA.DC.Jobs.Model/Enums/JobType.cs
+++ src/ESFA.DC.Jobs.Model/Enums/JobType.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ESFA.DC.Jobs.Model.Enums
+﻿namespace ESFA.DC.Jobs.Model.Enums
 {
+    /// <summary>
+    /// The enum representing the possible job types in the system.
+    /// This enum must be kept up to date with [JobManagement].[dbo].[JobType]
+    /// </summary>
     public enum JobType
     {
         IlrSubmission = 1,
         EasSubmission = 2,
         EsfSubmission = 3,
-        ReferenceData = 4,
-        PeriodEnd = 5
+        PeriodEnd = 20,
+        ReferenceDataEPA = 40,
+        ReferenceDataFCS = 41
     }
 }
